f1f0a5f213ba5d45f79d3760bb21cbb4354369e7
autodocs <autodocs>
2017-10-12 00:44:05 +0000

    build based on da8db01

--- stable/search_index.js
+++ stable/search_index.js
@@ -21,7 +21,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Home",
     "title": "Introduction",
     "category": "section",
-    "text": "REDUCE is a system for general algebraic computations of interest to mathematicians, scientists and engineers:exact arithmetic using integers and fractions;\narbitrary precision numerical approximation;\npolynomial and rational function algebra;\nfactorization and expansion of polynomials and rational functions;\ndifferentiation and integration of multi-variable functions;\nexponential, logarithmic, trigonometric and hyperbolic;\noutput of results in a variety of formats;\nautomatic and user controlled simplification of expressions;\nsubstitutions and pattern matching of expressions;\nquantifier elimination and decision for interpreted first-order logic;\nsolution of ordinary differential equations;\ncalculations with a wide variety of special (higher transcendental) functions;\ncalculations involving matrices with numerical and symbolic elements;\ngeneral matrix and non-commutative algebra;\npowerful intuitive user-level programming language;\ngenerating optimized numerical programs from symbolic input;\nDirac matrix calculations of interest to high energy physicists;\nsolution of single and simultaneous equations.Interface for applying symbolic manipulation on Julia expressions using REDUCE's term rewrite system:reduce expressions are RExpr objects that can parse into julia Expr objects and vice versa;\ninterface link communicates and interprets via various reduce output modes using rcall method;\nhigh-level reduce-julia syntax parser-generator walks arbitrary expression to rewrite mathematical code;\nimport operators from REDUCE using code generation to apply to arbitrary computational expressions."
+    "text": "REDUCE is a system for general algebraic computations of interest to mathematicians, scientists and engineers:exact arithmetic using integers and fractions;\narbitrary precision numerical approximation;\npolynomial and rational function algebra;\nfactorization and expansion of polynomials and rational functions;\ndifferentiation and integration of multi-variable functions;\nexponential, logarithmic, trigonometric and hyperbolic;\noutput of results in a variety of formats;\nautomatic and user controlled simplification of expressions;\nsubstitutions and pattern matching of expressions;\nquantifier elimination and decision for interpreted first-order logic;\nsolution of ordinary differential equations;\ncalculations with a wide variety of special (higher transcendental) functions;\ncalculations involving matrices with numerical and symbolic elements;\ngeneral matrix and non-commutative algebra;\npowerful intuitive user-level programming language;\ngenerating optimized numerical programs from symbolic input;\nDirac matrix calculations of interest to high energy physicists;\nsolution of single and simultaneous equations.Interface for applying symbolic manipulation on Julia expressions using REDUCE's term rewrite system:reduce expressions are RExpr objects that can parse into julia Expr objects and vice versa;\ninterface link communicates and interprets via various reduce output modes using rcall method;\nhigh-level reduce-julia syntax parser-generator walks arbitrary expression to rewrite mathematical code;\nimport operators from REDUCE using code generation to apply to arbitrary computational expressions;\ninteractive reduce> REPL within the Julia terminal window activated by } key."
 },
 
 {
@@ -45,7 +45,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Home",
     "title": "Usage",
     "category": "section",
-    "text": "Reduce expressions encapsulated into RExpr objects can be manipulated within julia using the standard syntax. Create an expression object either using the RExpr(\"expression\") string constructor or R\"expression\". Additionally, arbitrary julia expressions can also be parsed directly using the RExpr(expr) constructor. Internally RExpr objects are represented as an array that can be accessed by calling *.str[n] on the object.Sequences of reduce statements are automatically parsed into julia quote blocks using the RExpr constructor, which can parse back into a julia expression.julia> :((x+1+π)^2; int(1/(1+x^3),x)) |> RExpr\n\"**(+(x,1,pi),2); int(/(1,+(1,**(x,3))),x)\"\n\njulia> rcall(ans,:expand) |> parse\nquote\n    (2 * (x + 1) + π) * π + x ^ 2 + 2x + 1\n    -(((log((x ^ 2 - x) + 1) - 2 * log(x + 1)) - 2 * sqrt(3) * atan((2x - 1) // sqrt(3)))) // 6\nendCall split(::RExpr) to create a new RExpr object with all expressions split into separate array elements.The rcall method is used to evaluate any type of expression.julia> :(int(sin(im*x+pi)^2-1,x)) |> rcall\n:(-(((e ^ (4x) + 4 * e ^ (2x) * x) - 1)) // (8 * e ^ (2x)))The output of rcall will be the same as its input type.julia> \"int(sin(y)^2, y)\" |> rcall\n\"( - cos(y)*sin(y) + y)/2\"Use rcall(expr,switches...) to evaluate expr using REDUCE mode switches like :expand, :factor, and :latex.Mathematical operators and REDUCE modes can be applied directly to Expr and RExpr objects.julia> Expr(:function,:fun,:(return a^3+3*a^2*b+3*a*b^2+b^3)) |> factor\n:(function fun\n        return (a + b) ^ 3\n    end)Although not all language features have been implemented yet, it is possible to directly execute a variety of REDUCE style input programs using a synergy of julia syntax.julia> Expr(:for,:(i=2:34),:(product(i))) |> rcall\n:(@big_str \"295232799039604140847618609643520000000\")"
+    "text": "Reduce expressions encapsulated into RExpr objects can be manipulated within julia using the standard syntax. Create an expression object either using the RExpr(\"expression\") string constructor or R\"expression\". Additionally, arbitrary julia expressions can also be parsed directly using the RExpr(expr) constructor. Internally RExpr objects are represented as an array that can be accessed by calling *.str[n] on the object.Sequences of reduce statements are automatically parsed into julia quote blocks using the RExpr constructor, which can parse back into a julia expression.julia> :((x+im+π)^2; int(1/(1+x^3),x)) |> RExpr\n^(+(x,i,pi),2);\nint(/(1,+(1,^(x,3))),x);\n\njulia> rcall(ans,:expand) |> parse\nquote\n    (((π + 2x) * π + x ^ 2) - 1) + 2 * (π + x) * im\n    ((2 * sqrt(3) * atan((2x - 1) // sqrt(3)) - log((x ^ 2 - x) + 1)) + 2 * log(x + 1)) // 6\nendCall split(::RExpr) to create a new RExpr object with all expressions split into separate array elements.The rcall method is used to evaluate any type of expression.julia> :(int(sin(im*x+pi)^2-1,x)) |> rcall\n:(-(((e ^ (4x) + 4 * e ^ (2x) * x) - 1)) // (8 * e ^ (2x)))The output of rcall will be the same as its input type.julia> \"int(sin(y)^2, y)\" |> rcall\n\"( - cos(y)*sin(y) + y)/2\"Use rcall(expr,switches...) to evaluate expr using REDUCE mode switches like :expand, :factor, and :latex.Mathematical operators and REDUCE modes can be applied directly to Expr and RExpr objects.julia> Expr(:function,:(fun(a,b)),:(return a^3+3*a^2*b+3*a*b^2+b^3)) |> factor\n:(function fun(a, b)\n        return (a + b) ^ 3\n    end)Although not all language features have been implemented yet, it is possible to directly execute a variety of REDUCE style input programs using a synergy of julia syntax.julia> Expr(:for,:(i=2:34),:(product(i))) |> rcall\n:(@big_str \"295232799039604140847618609643520000000\")"
 },
 
 {
@@ -53,7 +53,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Home",
     "title": "Output mode",
     "category": "section",
-    "text": "Various output modes are supported. While in the REPL, the default nat output mode will be displayed for RExpr objects.julia> :(sin(x*im) + cos(y*φ)) |> RExpr\n\n      (sqrt(5) + 1)*y\n cos(-----------------) + sinh(x)*i\n             2This same output can also be printed to the screen by calling print(nat(r)) method.It is possible to direclty convert a julia expression object to LaTeX code using the latex method.julia> :(sin(x*im) + cos(y*φ)) |> latex |> print\n\\documentstyle{article}\n\\begin{document}\n\n\\begin{displaymath}\n\\cos \\left(\\left(\\left(\\sqrt {5}+1\\right) y\\right)/2\\right)+\\sinh \\,x\\: i\n\\end{displaymath}\n\n\\end{document}Internally, this command essentially expands to rcall(:(sin(x*im) + cos(y*φ)),:latex) |> print, which is equivalent.In IJulia the display output of RExpr objects will be rendered LaTeX with the rlfi REDUCE package in latex mode."
+    "text": "Various output modes are supported. While in the REPL, the default nat output mode will be displayed for RExpr objects.julia> :(sin(x*im) + cos(y*φ)) |> RExpr\n\n      (sqrt(5) + 1)*y\n cos(-----------------) + sinh(x)*i\n             2This same output can also be printed to the screen by calling print(nat(r)) method.It is possible to direclty convert a julia expression object to LaTeX code using the latex method.julia> :(sin(x*im) + cos(y*φ)) |> latex |> print\n\\begin{displaymath}\n\\cos \\left(\\left(\\left(\\sqrt {5}+1\\right) y\\right)/2\\right)+\\sinh \\,x\\: i\n\\end{displaymath}Internally, this command essentially expands to rcall(:(sin(x*im) + cos(y*φ)),:latex) |> print, which is equivalent.(Image: latex-equation)In IJulia the display output of RExpr objects will be rendered LaTeX with the rlfi REDUCE package in latex mode."
 },
 
 {
@@ -117,7 +117,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Library",
     "title": "Reduce.RExpr",
     "category": "Type",
-    "text": "A Reduce expression\n\nSummary:\n\ntype RExpr <: Any\n\nFields:\n\nstr :: Array{Compat.String,1}\n\n\n\n"
+    "text": "Reduce expression\n\nSummary:\n\ntype RExpr <: Any\n\nFields:\n\nstr::Array{Compat.String,1}\n\n\n\n"
 },
 
 {
@@ -125,7 +125,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Library",
     "title": "Reduce.rcall",
     "category": "Function",
-    "text": "rcall(r::RExpr)\n\nEvaluate a Reduce expression.\n\nExamples\n\njulia> R\"int(sin(x), x)\" |> RExpr |> rcall\n - cos(x)\n\n\n\nrcall{T}(e::T)\n\nEvaluate a Julia expression or string using the Reduce interpretor and convert output back into the input type\n\nExamples\n\njulia> rcall(\"int(sin(y)^2, y)\")\n\"( - cos(y)*sin(y) + y)/2\"\njulia> rcall(:(int(1/(1+x^2), x)))\n:(atan(x))\n\n\n\n"
+    "text": "rcall(r::RExpr)\n\nEvaluate a Reduce expression.\n\nExamples\n\njulia> R\"int(sin(x), x)\" |> RExpr |> rcall\n - cos(x)\n\n\n\nrcall{T}(e::T)\n\nEvaluate a Julia expression or string using the Reduce interpretor and convert output back into the input type\n\nExamples\n\njulia> rcall(\"int(sin(y)^2, y)\")\n\"( - cos(y)*sin(y) + y)/2\"\n\njulia> rcall(:(int(1/(1+x^2), x)))\n:(atan(x))\n\n\n\n"
 },
 
 {
@@ -133,7 +133,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Library",
     "title": "Base.parse",
     "category": "Function",
-    "text": "parse(r::RExpr)\n\nParse a Reduce expression into a Julia expression\n\nExamples\n\njulia> parse(R\"sin(i*x)\")\n:(sinh(x) * im)\n\n\n\n"
+    "text": "parse(r::RExpr)\n\nParse a Reduce expression into a Julia expression\n\nExamples\n\njulia> parse(R\"sin(i*x)\")\n:(sin(im * x))\n\n\n\n"
 },
 
 {
@@ -145,11 +145,35 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
+    "location": "library.html#Reduce-Interface-1",
+    "page": "Library",
+    "title": "Reduce Interface",
+    "category": "section",
+    "text": "Reduce.ResetRExprrcallparseload_package"
+},
+
+{
+    "location": "library.html#Imported-Operators-1",
+    "page": "Library",
+    "title": "Imported Operators",
+    "category": "section",
+    "text": "Reduce switch modes callable as functions from Julia:expand, :complex, :factor, :expandlog, :combinelog, :precise, :combineexpt, :rounded, :evallhseq, :nat, :latexCalculus operators:df, :intUnary operators:abs, :conj, :factorial, :floor, :max, :min, :round, :sign, :acos, :acosh, :acot, :acoth, :acsc, :acsch, :asec, :asech, :asin, :asinh, :atan, :atanh, :atan2, :cos, :cosh, :cot, :coth, :csc, :csch, :exp, :hypot, :log, :log10, :sec, :sech, :sin, :sinh, :sqrt, :tan, :tanh, :gamma, :factorize:beta, :besseli, :besselj, :besselk, :bessely, :polygamma, :zeta:ibeta, :igamma, :ln, :psi, :bernoulli, :continued_fraction, :ci, :dilog, :ei, :si, :airy_ai, :airy_aiprime, :airy_bi, :airy_biprime, :hanekl1, :hankel2, :kummerm, :kummeru, :lommel1, :lommel2, :struveh, :struvel, :whittakerm, :whittakeru, :solidharmonicy, :sphericalharmonicy:ceiling, :fix, :impart, :repart, :nextprime, :euler, :fibonacci, :motzkin, :random, :random_new_seed"
+},
+
+{
     "location": "library.html#Reduce.parsegen",
     "page": "Library",
     "title": "Reduce.parsegen",
     "category": "Function",
     "text": "parsegen(::Symbol,::Symbol)\n\nParser generator that outputs code to walk and manipulate REDUCE expressions\n\n\n\n"
+},
+
+{
+    "location": "library.html#Reduce.linefilter",
+    "page": "Library",
+    "title": "Reduce.linefilter",
+    "category": "Function",
+    "text": "linefilter(::Expr)\n\nRecursively filters out :line blocks from Expr objects\n\n\n\n"
 },
 
 {
@@ -169,11 +193,11 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
-    "location": "library.html#Functions-1",
+    "location": "library.html#Tools-and-Options-1",
     "page": "Library",
-    "title": "Functions",
+    "title": "Tools & Options",
     "category": "section",
-    "text": "Reduce.ResetRExprrcallparseload_packageReduce.parsegenReduce.RationalReduce.SubCall"
+    "text": "Reduce.parsegenReduce.linefilterReduce.RationalReduce.SubCall"
 },
 
 ]}
