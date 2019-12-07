6c72dbbf3ef86244ae7c7670866bb5d10dc25b63
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-12-07 18:51:10 +0000

    build based on 58b1c58

--- versions.js
+++ versions.js
@@ -1,12 +1,7 @@
 var DOC_VERSIONS = [
   "stable",
-  "latest",
-  "release-0.4",
-  "release-0.3",
-  "v0.4.1",
-  "v0.4.0",
-  "v0.3.2",
-  "v0.2.2",
-  "v0.2.1",
-  "v0.2.0",
+  "v0.4",
+  "v0.3",
+  "v0.2",
+  "dev",
 ];
